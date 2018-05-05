43f7b91cd174528f318d28cf5da756e121ea7722
Tommy DiLillo <tommydilillo@gmail.com>
2018-05-05 17:41:43 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,112 @@
+window.onload = function() {
+  //global variables
+  var currentGame;
+  var currentPlayer;
+
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+
+  // game constructor function
+  var Game = function() {
+    console.log("this: " + this);
+    this.player = {};
+    this.obstacles = [];
+  };
+
+  // Player constructor functiom
+  var Player = function() {
+    this.x = 100;
+    this.y = 100;
+    this.r = 20;
+    this.startAngle = 0;
+    this.endAngle = 2 * Math.PI;
+    //   clockwise: anti - clockwise;
+    this.boolean = true;
+  };
+  //Draws Player on the board
+  Player.prototype.drawPlayer = function() {
+    console.log("hey: ", this);
+    ctx.beginPath();
+    ctx.arc(
+      this.x,
+      this.y,
+      this.r,
+      this.startAngle,
+      this.endAngle,
+      // this.clockwise,
+      this.boolean
+    );
+    ctx.fillStyle = "green";
+    ctx.fill();
+  };
+  //Defining the player move options
+  Player.prototype.move = function(clickedKey) {
+    // console.log(this.x);
+    // ctx.clearRect(0 - this.r, 0 - this.r, this.r * 2, this.r * 2); // NEED TO ACCESS this.x, etc.
+    switch (clickedKey) {
+      case 37:
+        console.log("left");
+        this.x -= 10;
+        break;
+      case 38:
+        console.log("up");
+        this.y -= 10;
+        break;
+      case 39:
+        console.log("right");
+        this.x += 10;
+        break;
+      case 40:
+        console.log("down");
+        this.y += 10;
+        break;
+      default:
+        console.log("You're pressing the wrong button");
+    }
+    this.drawPlayer();
+  };
+
+  var Obstacle = function() {
+    this.x = 100;
+    this.y = 100;
+    this.r = 50;
+    this.startAngle = 0;
+    this.endAngle = 2 * Math.PI;
+    //   clockwise: anti - clockwise,
+    this.boolean = true;
+  };
+
+  Obstacle.prototype.drawObstacle = function() {
+    console.log("hey: ", this);
+    ctx.beginPath();
+    ctx.arc(
+      this.x,
+      this.y,
+      this.r,
+      this.startAngle,
+      this.endAngle,
+      // this.clockwise,
+      this.boolean
+    );
+    ctx.fillStyle = "red";
+    ctx.fill();
+  };
+
+  function startGame() {
+    console.log("start-game works");
+    currentGame = new Game();
+    currentPlayer = new Player();
+    currentGame.player = currentPlayer;
+    currentGame.player.drawPlayer();
+
+    // currentGame.update()
+  }
+
+  startGame();
+
+  document.onkeydown = function(e) {
+    var clickedKey = e.keyCode;
+    console.log(clickedKey);
+    currentGame.player.move(clickedKey);
+  };
+};
